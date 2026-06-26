b4f7d5c9bc83e38fbfd4402087d2451017a1863a
DavidSun11 <168669013+DavidSun11@users.noreply.github.com>
2026-06-26 11:57:50 -0400

    chore: migrate from SQLite to PostgreSQL and configure Vercel deployment settings

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -1,10 +1,12 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      PAYLOAD_SECRET: string
       DATABASE_URL: string
+      PAYLOAD_SECRET: string
       NEXT_PUBLIC_SERVER_URL: string
       VERCEL_PROJECT_PRODUCTION_URL: string
+      CRON_SECRET: string
+      PREVIEW_SECRET: string
     }
   }
 }
